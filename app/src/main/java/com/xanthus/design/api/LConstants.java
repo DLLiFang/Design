d93bb9f5a57ae1f354a032499c2b69f4ffc6ee55
Dyclf <lifang@dengyuecang.com>
2016-11-22 00:42:10 +0800

    c zasdfasdfsadfasdfsadfas

--- app/src/main/java/com/xanthus/design/api/LConstants.java
+++ app/src/main/java/com/xanthus/design/api/LConstants.java
@@ -6,6 +6,6 @@ package com.xanthus.design.api;
 
 public interface LConstants {
     //String DOMAIN = "http://192.168.1.105:8080/v1/";
-    String DOMAIN = "http://192.168.123.232:8080/v1/";
+    String DOMAIN = "http://192.168.123.232:8080/v1/";//ip换成你的，跑到手机上，手机跟电脑处于同一局域网就能访问接口了
     String FILE_PRE = "http://192.168.123.232:8080/v1/file/";
 }
